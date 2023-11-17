0daf0a5bdbec4e6122ea662513b2733043c226d5
Samirgorai <samirgorai17@gmail.com>
2023-11-17 12:12:12 +0530

    Added admin login created superuser to login in admin added admin  anchor tag to login to admin page added database model employee
    Signed-off-by: Samir Gorai <samirgorai17@gmail.com>

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Employee(models.Model):
+    employee_name=models.CharField(max_length=255)
+    employee_ID=models.IntegerField()
+    employee_joined_date=models.DateField()
+    employee_monthly_salary=models.IntegerField()
+    employee_adress=models.CharField(max_length=255)
+    employee_designation=models.CharField(max_length=20)
+
+    def __str__(self) -> str:
+        return self.employee_name
+    
+    
